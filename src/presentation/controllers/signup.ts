8685c7387d7465a3a452a4e45db96ecee44ded0a
Claudio Tomas <ccaducarlos@gmail.com>
2024-04-11 11:27:12 +0200

    test: same as before

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -2,7 +2,7 @@ export class SignUpController {
   handle(httpRequest: unknown) {
     return {
       statusCode: 400,
-      body: new Error('Missing a param: name'),
+      body: new Error('Missing param: name'),
     }
   }
 }
