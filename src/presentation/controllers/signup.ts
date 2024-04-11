e5ad3e456106342089641ffd1992dfd8d5fdf896
Claudio Tomas <ccaducarlos@gmail.com>
2024-04-11 14:54:12 +0200

    refactor: replace multiple if blocks to use a repetition block to check missing fiels

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -3,14 +3,12 @@ import { badRequest } from '../helpers/http-helper'
 import { httpRequest, HttpResponse } from '../protocols/http'
 export class SignUpController {
   handle(httpRequest: httpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
+    const requiredFields = ['name', 'email']
+    for (const field of requiredFields) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(`${field}`))
+      }
     }
-
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-
     return {
       statusCode: 200,
       body: { message: 'Success' },
