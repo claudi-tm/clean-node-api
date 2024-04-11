3c86469407aa7f6269b20f8f8e7f3b0686857f96
Claudio Tomas <ccaducarlos@gmail.com>
2024-04-11 12:00:59 +0200

    feat: add email check in test

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -14,4 +14,16 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
+  test('should Should return 400 if no email is provided', () => {
+    const sut = new SignUpController()
+    console.log(sut)
+    const httpRequest = {
+      name: 'any_name',
+      password: 'password',
+      passwordConfirmation: 'password',
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: email'))
+  })
 })
