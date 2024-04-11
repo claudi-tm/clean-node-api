7240db8e1ad0f837f2f91976a2253efc24547a60
Claudio Tomas <ccaducarlos@gmail.com>
2024-04-11 12:42:08 +0200

    feat: ensure the tests looks for both name and email

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -5,22 +5,25 @@ describe('SignUp Controller', () => {
     const sut = new SignUpController()
     console.log(sut)
     const httpRequest = {
-      name: 'any_name',
-      email: 'any_email@yahoo.com',
-      password: 'password',
-      passwordConfirmation: 'password',
+      body: {
+        email: 'any_email@yahoo.com',
+        password: 'password',
+        passwordConfirmation: 'password',
+      },
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: name'))
   })
-  test('should Should return 400 if no email is provided', () => {
+  test('should Should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     console.log(sut)
     const httpRequest = {
-      name: 'any_name',
-      password: 'password',
-      passwordConfirmation: 'password',
+      body: {
+        name: 'any_name',
+        password: 'password',
+        passwordConfirmation: 'password',
+      },
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
