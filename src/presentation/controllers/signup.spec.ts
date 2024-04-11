270e9084126cd0d97076214b86f2b663b4931c94
Claudio Tomas <ccaducarlos@gmail.com>
2024-04-11 10:57:00 +0200

    test: test

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,15 @@
+describe('SignUp Controller', () => {
+  test('should Should return 400 if no name is provided', () => {
+    // const sut = new SignUpController()
+    // const httpRequest = {
+    //   body: {
+    //     name: 'any_name',
+    //     email: 'any_email@yahoo.com',
+    //     password: 'password',
+    //     passwordConfirmation: 'password',
+    //   },
+    // }
+    // const httpResponse = sut.handle(httpRequest)
+    // expect(httpResponse.statusCode).toBe(400)
+  })
+})
